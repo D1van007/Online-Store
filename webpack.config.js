17af1422c06316acb08cea4f7054e9be3cb4d9c1
D1van007 <d1van28.01.1987@gmail.com>
2022-12-16 22:39:43 +0300

    fix: also fix git branch and start task

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const CopyPlugin = require("copy-webpack-plugin");
+require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      { test: /\.txt$/, use: 'raw-loader' },
+      {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: 'style-loader',
+            options: { injectType: 'singletonStyleTag' },
+          },
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    // new CopyPlugin({
+    //   patterns: [
+    //     { from: "redirect", to: "" },
+    //   ],
+    // }),
+  ],
+  resolve: {
+    extensions: [ ".tsx", ".ts", ".js" ]
+  },
+};
