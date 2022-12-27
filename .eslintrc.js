55d960544e2a49706273656ca317b954150e7f1a
D1van007 <d1van28.01.1987@gmail.com>
2022-12-27 20:27:55 +0300

    refactor: start add airbnb, but stoped before merge develop brunch

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,23 @@
-// eslint-disable-next-line no-undef
+
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
     },
     "extends": [
         "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:import/typescript",
+        "plugin:@typescript-eslint/recommended",
+        // "airbnb",
+        // "airbnb-typescript"
     ],
     "overrides": [
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaVersion": "latest",
-        "sourceType": "module"
+        "sourceType": "module",
+        "project": "./tsconfig.json"
     },
     "plugins": [
         "@typescript-eslint"
