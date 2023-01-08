98ecdacc87e46eaf34b54f5641805777095a54d6
D1van007 <d1van28.01.1987@gmail.com>
2023-01-09 01:11:04 +0300

    feat: creat promocode in cart

--- src/components/cart/cart_module/totalBuyCart.ts
+++ src/components/cart/cart_module/totalBuyCart.ts
@@ -1,5 +1,6 @@
 import { OrderForm } from '../../order_form/order_form';
 import { Cart } from './cart';
+import { LocalCart } from './localCart';
 
 export class TotalBuy {
   selectorParentsSum: HTMLElement;
@@ -7,19 +8,20 @@ export class TotalBuy {
   orederForm: OrderForm | null = null;
   cart: Cart;
   promoInputDOM!: HTMLButtonElement;
-  promoRSInputDOM!: HTMLElement;
-  promoEPMInputDOM!: HTMLElement;
-  promoRSBtnAddDOM!: HTMLButtonElement;
   promoFormDOM!: HTMLElement;
-  promoRSAppliedDOM!: HTMLElement;
-  promoEPMAppliedDOM!: HTMLElement;
+  promoContentValidDOM!: HTMLElement;
+  promoAppliedDOM!: HTMLElement;
+  promoTotalPriceDOM!: HTMLElement;
+  localCart: LocalCart;
   constructor(selectorParentsSum: HTMLElement, cart: Cart) {
+    this.localCart = new LocalCart();
     this.cart = cart;
     this.selectorParentsSum = selectorParentsSum;
     this.renderTotalBuy();
     this.initDOM();
     this.buyNowButtonEvent();
     this.promoInputEvent();
+    this.promoBtnEvent();
   }
 
   renderTotalBuy() {
@@ -33,54 +35,96 @@ export class TotalBuy {
     });
   }
   initDOM() {
+    this.promoContentValidDOM = this.selectorParentsSum.querySelector('.promo__content--valid') as HTMLElement;
     this.promoInputDOM = this.selectorParentsSum.querySelector('#promo__input') as HTMLButtonElement;
-    this.promoRSInputDOM = this.selectorParentsSum.querySelector('.promo__input--rs') as HTMLElement;
-    this.promoEPMInputDOM = this.selectorParentsSum.querySelector('.promo__input--epm') as HTMLElement;
-    this.promoRSBtnAddDOM = this.selectorParentsSum.querySelector('.promo__input--rs-add') as HTMLButtonElement;
+    this.promoTotalPriceDOM = this.selectorParentsSum.querySelector('#cart-total--price') as HTMLElement;
     this.promoFormDOM = this.selectorParentsSum.querySelector('.cart-total--promo') as HTMLElement;
-    this.promoEPMAppliedDOM = this.selectorParentsSum.querySelector('.promo__input--epm-applied') as HTMLElement;
+    this.promoAppliedDOM = this.selectorParentsSum.querySelector('.promo__input--applied') as HTMLElement;
   }
-  renderPromo(code: string) {
-    // if(code)
+
+  renderPromo(objWithPromo: { [x: string]: string }, code: string) {
+    this.promoContentValidDOM.insertAdjacentHTML('beforeend', createHTMLPromo(objWithPromo, code));
   }
+
   promoInputEvent() {
     this.promoInputDOM.addEventListener('input', () => {
-      console.log(Object.keys(promo));
-      if (this.promoInputDOM.value.toLocaleUpperCase() === 'RS') {
-        this.promoRSInputDOM.classList.add('active');
-      } else if (this.promoInputDOM.value.toLocaleUpperCase() === 'EPM') {
-        this.promoEPMInputDOM.classList.add('active');
-      } else {
-        this.promoRSInputDOM.classList.remove('active');
-        this.promoEPMInputDOM.classList.remove('active');
+      const inputValue = this.promoInputDOM.value.toLocaleLowerCase();
+      if (this.selectorParentsSum.querySelector(`.promo__content--${inputValue}-add`) as HTMLElement) {
+        return;
       }
+      if (Object.keys(promo).some(e => e === inputValue)) {
+        this.renderPromo(promo, this.promoInputDOM.value.toLocaleLowerCase());
+        if (this.selectorParentsSum.querySelector(`.promo__input--${inputValue}-drop`)) {
+          document.querySelector(`.promo__content--${inputValue}-btn`)?.remove();
+        }
+      } else this.promoContentValidDOM.innerHTML = '';
     });
+  }
+
+  promoBtnEvent() {
     this.selectorParentsSum.addEventListener('click', event => {
-      console.log(<HTMLElement>event.target);
       event.preventDefault();
-      if ((<HTMLElement>event.target).matches('.promo__input--rs-add')) {
-        this.renderPromoAppl('rs');
-        this.promoRSBtnAddDOM.classList.remove('active');
+      const inputValue = (<HTMLElement>event.target)?.dataset.promo as string;
+      if ((<HTMLElement>event.target).matches(`.promo__content--${inputValue}-btn`)) {
+        if (!document.querySelector('.promo__input--applied')) {
+          this.promoFormDOM.insertAdjacentHTML('beforebegin', createHTMLPromoApplyContainer());
+        }
+        this.renderPromoAppl(promo, inputValue);
+        this.addSale();
+        (this.selectorParentsSum.querySelector(`.promo__content--${inputValue}-btn`) as HTMLElement).style.display =
+          'none';
       }
-      if ((<HTMLElement>event.target).matches('.promo__input--rs-drop')) {
-        console.log('enter0');
-        this.removePromoAppl('rs');
-        this.promoRSBtnAddDOM.classList.add('active');
+      if ((<HTMLElement>event.target).matches(`.btn-drop`)) {
+        this.removePromoAppl((<HTMLElement>event.target).dataset.promo as string);
+        this.addSale();
+        if (this.selectorParentsSum.querySelector(`.promo__content--${inputValue}-btn`)) {
+          (this.selectorParentsSum.querySelector(`.promo__content--${inputValue}-btn`) as HTMLElement).style.display =
+            'block';
+        }
+        if (this.selectorParentsSum.querySelector('.promo__input--applied')?.children.length === 1) {
+          this.selectorParentsSum.querySelector('.promo__input--applied')?.remove();
+        }
       }
     });
   }
 
-  renderPromoAppl(code: string) {
-    this.promoFormDOM.insertAdjacentHTML('beforebegin', createHTMLPromoApply(code));
+  addSale() {
+    const everyDiscont = this.selectorParentsSum.querySelectorAll('.discount-text');
+    const totalPrice = this.localCart.getLocalTotalPrice();
+    const arrDiscont: (string | undefined)[] = [];
+    const newPriceContainerDOM = this.selectorParentsSum.querySelector(
+      '.cart-total--new-price__container',
+    ) as HTMLElement;
+    everyDiscont.forEach(e => arrDiscont.push(e.textContent?.split(' - ')[1].slice(0, -1)));
+    if (arrDiscont.length < 1) {
+      this.promoTotalPriceDOM.style.textDecoration = 'none';
+      newPriceContainerDOM.innerHTML = '';
+      return;
+    } else {
+      this.promoTotalPriceDOM.style.textDecoration = 'line-through';
+      const newTotalPrice =
+        totalPrice -
+        (totalPrice *
+          (arrDiscont as unknown as number[]).reduce((acc: number, e: number) => {
+            return +e + +acc;
+          })) /
+          100;
+      newPriceContainerDOM.innerHTML = `<p class="cart-total--new-price">Total: €${newTotalPrice}</p>`;
+    }
+  }
+
+  renderPromoAppl(objWithPromo: { [x: string]: string }, code: string) {
+    this.promoAppliedDOM = this.selectorParentsSum.querySelector('.promo__input--applied') as HTMLElement;
+    this.promoAppliedDOM.insertAdjacentHTML('beforeend', createHTMLPromoApply(objWithPromo, code));
   }
 
   removePromoAppl(code: string) {
-    this.promoRSAppliedDOM = this.selectorParentsSum.querySelector('.promo__input--rs-applied') as HTMLElement;
-    if (code === 'rs') {
-      this.promoRSAppliedDOM.remove();
-    }
-    if (code === 'epm') {
-      this.promoEPMAppliedDOM.remove();
+    const promoAppliedContentDOM = this.selectorParentsSum.querySelector(
+      `.promo__input--${code}-content`,
+    ) as HTMLElement;
+    const promoAppliedBtnDOM = this.selectorParentsSum.querySelector(`.promo__input--${code}-drop`) as HTMLElement;
+    if (code === promoAppliedBtnDOM.dataset.promo) {
+      promoAppliedContentDOM.remove();
     }
   }
 }
@@ -93,36 +137,35 @@ function createHTMLTotalBuy(): string {
         <div>
             <input id="promo__input" type="text" name="text" placeholder="Enter promo code('RS', 'EPM')"/>
         </div>
-        <div class="promo__input--rs">
-            <span class="promo__input--rs-lable">Rolling Scopes School - 10%</span>
-            <button class="promo__input--rs-add active">ADD</button>
-        </div>
-        <div class="promo__input--epm">
-            <span class="promo__input--epm-lable">EPAM Systems - 10%</span>
-            <button class="promo__input--epm-add active">ADD</button>
-        </div>
+        <div class="promo__content--valid"></div>
         <button class="buy-now">Buy Now</button>
     </form>
 `;
 }
 
-function createHTMLPromoApply(code: string) {
-  const textSale = () => {
-    if (code === 'rs') {
-      return promo.rs;
-    }
-    if (code === 'epm') {
-      return promo.epm;
-    }
-  };
-  return `
-        <div class="promo__input--${code}-applied">
-            <span class="promo__input--${code}-lable">Applied codes</span>            
-            <span class="promo__input--${code}-text">
-            ${textSale()}</span>
-            <button class="promo__input--${code}-drop">DROP</button>
+function createHTMLPromoApply(objWithPromo: { [x: string]: string }, code: string) {
+  return `<div class="promo__input--${code}-content">
+        <p class="promo__input--${code}-text discount-text">${objWithPromo[code]}</p>
+        <button class="promo__input--${code}-drop btn-drop" data-promo="${code}">DROP</button>
         </div>
 `;
+}
+function createHTMLPromoApplyContainer(): string {
+  return `<div class="cart-total--new-price__container">
+          </div>
+        <div class="promo__input--applied">
+            <h4 class="promo__input--applied-title">Applied codes</h4>            
+        </div>
+`;
+}
+
+function createHTMLPromo(objWithPromo: { [x: string]: string }, code: string) {
+  return `
+  <div class="promo__content--${code}-add">
+    <span class="promo__content--${code}-lable">${objWithPromo[code]}</span>
+    <button class="promo__content--${code}-btn btn-add" data-promo="${code}">ADD</button>
+  </div>
+  `;
 }
 
 const promo = {
