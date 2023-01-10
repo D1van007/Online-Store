547c6fcc2b8639ac4a9df5ffa0b86d7214840af5
kivlaros <kivlaros@gmail.com>
2023-01-10 03:36:48 +0300

    fix: custom event type

--- src/components/router/events_history.ts
+++ src/components/router/events_history.ts
@@ -1,4 +1,4 @@
-export function eventedPushState(state: any, title: string, url: string) {
+export function eventedPushState(state: unknown, title: string, url: string) {
   const pushChangeEvent = new CustomEvent('onpushstate', {
     detail: {
       state,
